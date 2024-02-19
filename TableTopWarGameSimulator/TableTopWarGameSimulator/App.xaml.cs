4dc7674ae09cfc14ee0795a6cb99a2269904417e
Martin Westerhof <a.m.westerhof@gmail.com>
2024-02-19 17:55:33 +0100

    Project Setup

---
+++ TableTopWarGameSimulator/TableTopWarGameSimulator/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace TableTopWarGameSimulator
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
