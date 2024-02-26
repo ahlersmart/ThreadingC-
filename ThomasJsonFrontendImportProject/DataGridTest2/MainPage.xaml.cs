ee86bf0d758c60867c2350c5af1df04c8cdeece5
Igneous <tho09@live.nl>
2024-02-26 02:15:41 +0100

    Added Json Importer as Loose Project
    Added my custom project which is to be merged with the main project manually when the JSON file is available. Discussion on structuring and such are required.

---
+++ ThomasJsonFrontendImportProject/DataGridTest2/MainPage.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.ObjectModel;
+using System.Net.Http.Json;
+
+namespace DataGridTest2
+{
+    public partial class MainPage : ContentPage
+    {
+        private readonly HttpClient httpClient = new();
+
+        public bool IsRefreshing { get; set; }
+        public ObservableCollection<Monkey> Monkeys { get; set; } = new();
+        public Command RefreshCommand { get; set; }
+        public Monkey SelectedMonkey { get; set; }
+
+        public MainPage()
+        {
+            RefreshCommand = new Command(async () =>
+            {
+                await Task.Delay(2000);
+
+                await LoadMonkeys();
+
+                IsRefreshing = false;
+                OnPropertyChanged(nameof(IsRefreshing));
+            });
+
+            BindingContext = this;
+
+            InitializeComponent();
+        }
+
+        protected async override void OnNavigatedTo(NavigatedToEventArgs args) 
+        {
+            base.OnNavigatedTo(args);
+
+            await LoadMonkeys();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e) 
+        {
+            Monkeys.Clear();
+        }
+
+        private async Task LoadMonkeys()
+        {
+            var monkeys = await httpClient.GetFromJsonAsync<Monkey[]>("https://montemagno.com/monkeys.json");
+
+            Monkeys.Clear();
+
+            foreach (Monkey monkey in monkeys)
+            {
+                Monkeys.Add(monkey);
+            }
+        }
+    }
+}
